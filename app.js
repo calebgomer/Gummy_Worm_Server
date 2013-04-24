40fb8c41e0a2f85a33fd1b65cb5015861e0daed7
Caleb Gomer <calebgomer@gmail.com>
2013-04-24 03:40:00 -0400

    4 player (communication) working

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
   , http = require('http')
   , io = require('socket.io')
   , path = require('path');
@@ -36,15 +35,12 @@ app.configure(function(){
   app.use(app.router);
 });
 
-//DEV?
+//use the express error handler in development mode
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-//basic routes
-app.get('/gummyworm', function(req, res) {
-  res.sendfile(__dirname + '/gummyworm/play.html');
-});
+//pretty much anything you do will send the game play files
 app.get('*', function(req, res) {
   res.sendfile(__dirname + '/gummyworm/play.html')
 });
@@ -60,25 +56,9 @@ server.listen(app.get('port'), function(){
 var io = io.listen(server);
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+  io.set("polling duration", 10);
+  io.set('log level', 1);
 });
-
-var Directions = {
-  posX: 1,
-  negX: 2,
-  posY: 3,
-  negY: 4,
-  posZ: 5,
-  negZ: 6,
-  length: 6
-}
-
-function Coordinate(x, y, z, direction) {
-  this.x = x || Math.floor(Math.random()*10);
-  this.y = y || Math.floor(Math.random()*10);
-  this.z = z || Math.floor(Math.random()*10);
-  this.direction = direction || Math.floor(Math.random()*Directions.length)+1;
-}
 
 var players = [];
 var playerSpots = {};
@@ -88,14 +68,8 @@ function Player(socket, name, color, location) {
   this.name = name;
   this.color = color;
   this.location = location;
+  this.ready = false;
 }
-
-var _players = {
-  1: { x: 000, y: 000, z: 000, up: '+y', dir: '+z' },
-  2: { x: 100, y: 000, z: 000, up: '+y', dir: '+z' },
-  3: { x: 100, y: 000, z: 000, up: '+x', dir: '+y' },
-  4: { x: 000, y: 000, z: 000, up: '+y', dir: '+x' }
-};
 
 var gameTickId;
 
@@ -112,12 +86,17 @@ var gameTickId;
 // }
 
 function addPlayer(socket, data) {
-  //calculate random location
+
+  console.log('new player');
+
   var location = {};
   location.y = 0;
   location.up = 'y';
   location.isUpPos = true;
+
+  //for now all players start at the same place
   switch(players.length) {
+
     case 0:
       location.x = 0;
       location.z = 0;
@@ -126,6 +105,7 @@ function addPlayer(socket, data) {
       location.left = 'x';
       location.isLeftPos = false;
       break;
+
     case 1:
       location.x = 0;
       location.z = 99;
@@ -134,6 +114,7 @@ function addPlayer(socket, data) {
       location.left = 'z';
       location.isLeftPos = false;
       break;
+
     case 2:
       location.x = 99;
       location.z = 99;
@@ -142,6 +123,7 @@ function addPlayer(socket, data) {
       location.left = 'x';
       location.isLeftPos = true;
       break;
+
     case 3:
       location.x = 99;
       location.z = 0;
@@ -151,53 +133,75 @@ function addPlayer(socket, data) {
       location.isLeftPos = true;
       break;
   }
-  
-  console.log('new player');
 
+  //save off this player's location in the list
   playerSpots[socket.id] = players.length;
-  players.push(new Player(socket, data.name, data.color, location ));
 
+  //push player into the list
+  players.push(new Player(socket, data.name, data.color, location));
+
+  //send player their starting location
   socket.emit('start-details', location);
 
-  console.log('****LEN****',players.length);
-  if (players.length >= 1)
-    gameTickId = setInterval(loop, 750);
+  console.log('# players',players.length);
 }
 
+
+//wait for all players to be ready
+var numReady = 0;
+function playerReady(socket) {
+
+  if (players[playerSpots[socket.id]].ready == false) {
+    players[playerSpots[socket.id]].ready = true;
+    console.log('player ',playerSpots[socket.id]+1,' is ready');
+    numReady++;
+  }
+
+  if (numReady == players.length)
+    startGame();
+}
+
+
+//start the game timer
+function startGame() {
+  if (players.length >= 1)
+    gameTickId = setInterval(tick, 250/4);
+}
+
+
+//handle new user connections
 io.sockets.on('connection', function(socket) {
+
   //welcome new player, give them their ID
-  socket.emit('send-details', { });
-  // socket.emit('send-location', { id : socket.id });
+  socket.emit('send-details', { /* 'nuff said */ });
 
   //get player details
   socket.on('player-details', function(data) { addPlayer(socket, data); });
 
+  //player is ready to go
+  socket.on('ready-to-go', function() { playerReady(socket); });
+
   //player requests move
   socket.on('request-move', function(data) { handleMoves(socket, data); });
 
-  //broadcast the player's location
-  // socket.on('location', function(data) {
-  //   socket.volatile.broadcast.emit('location-update', data);
-  // });
-
-  socket.on('destroyed', function(data) {
-    socket.broadcast.emit('destroyed', data);
-  });
-
+  //figure out how best to handle a disconnect?
   socket.on('disconnect', function() {
     console.log('***player#',socket.id,'disconnected');
-    players.splice(playerSpots[socket.id],1);
+    if (playerSpots[socket.id])
+      players.splice(playerSpots[socket.id], 1);
   });
+
+  //figure out how best to handle a disconnect?
   socket.on('bye', function() {
     console.log('***BYE***player#',socket.id,'disconnected');
-    players.splice(playerSpots[socket.id],1);
+    socket.broadcast.emit('player-left', {/* fill in later */} );
+    if (playerSpots[socket.id])
+      players.splice(playerSpots[socket.id], 1);
   });
 });
 
-// Position.prototype.clone = function() {
-//   return new Position(this.x, this.y, this.z, this.up, this.isUpPos, this.dir, this.isDirPos, this.left, this.isLeftPos);
-// };
 
+//process and make decisions on move requests
 function handleMoves(socket, data) {
   console.log(data);
 
@@ -216,6 +220,7 @@ function handleMoves(socket, data) {
   newDirection.isLeftPos = player.isLeftPos;
 
   switch(data.direction) {
+
     case 'left':
       newDirection.dir = player.left;
       newDirection.isDirPos = player.isLeftPos;
@@ -245,88 +250,76 @@ function handleMoves(socket, data) {
       break;
   }
 
+  //put the new direction in the data to send back
   data.newDirection = newDirection;
-  console.log(data.newDirection);
+  //update the player's location/direction with the new one
+  players[playerSpots[socket.id]].location = newDirection;
 
   socket.emit('move-approved', data);
 }
 
-
+//milliseconds the game started
 var startTime = null;
 
-//game loop!
-function loop() {
+//player to update this tick
+var personToTick = 0;
+
+//game seconds!
+function tick() {
+
+  //initialize the game starting time
   if (startTime == null)
     startTime = Math.round(new Date().getTime());
 
-  // console.log('**START**',startTime);
-
-  // if (_players.length < 2)
-  //   clearInterval(gameTickId);
-
-
+  //milliseconds since the game started
   var now = Math.round(new Date().getTime() - startTime);
 
-  // if (now % 1000 <= 10)
-  //   for (var a in gameBoard)
-  //     for (var b in gameBoard[a])
-  //       for (var c in gameBoard[a][b])
-  //         if (gameBoard[a][b][c])
-  //           console.log(gameBoard[a][b][c]);
+  //locations of each player, will be sent to the one lucky player this tick
+  var playerLocations = [];
 
-  // console.log('****NOW**',now);
+  //move each player 1 block on this tick
+  for (var i in players) {
+    var player = players[i];
+    playerLocations.push(player.location);
 
-  // console.log('gameboard',gameBoard.length);
-  // console.log('gameboard',gameBoard[0].length);
-  // console.log('gameboard',gameBoard[0][0].length);
+    switch (player.location.dir) {
 
+      case 'x':
+        player.location.x += player.location.isDirPos ? 1 : -1;
+        break;
 
+      case 'y':
+        player.location.y += player.location.isDirPos ? 1 : -1;
+        break;
 
-  // for (var x = 1; x < 5; x++) {
-  //   // console.log('x************',x,_players[x].dir);
-  //   var pos = _players[x].dir[0] == '+';
-  //   var dir = _players[x].dir[1];
-  //   switch (dir) {
-  //     case 'x':
-  //       _players[x].x += pos ? 1:-1;
-  //       break;
+      case 'z':
+        player.location.z += player.location.isDirPos ? 1 : -1;
+        break;
+    }
 
-  //     case 'y':
-  //       _players[x].y += pos ? 1:-1;
-  //       break;
+    // if (i == 1)
+      console.log(player.location.x, player.location.y, player.location.z, 'moving', player.location.isDirPos?'+':'-',player.location.dir,'direction');
+  }
+  console.log('');
 
-  //     case 'z':
-  //       _players[x].z += pos ? 1:-1;
-  //       break;
-  //   }
+  //update one player this tick
+  if (players[personToTick] && players[personToTick].socket)
+    players[personToTick].socket.emit('tick', { now: now, players: playerLocations } );
 
-
-  //   console.log(x,'(',_players[x].x,',',_players[x].y,',',_players[x].z,')');
-  //   // gameBoard[_players[x].x][_players[x].y][_players[x].z] = x+1;
-  //   // var i = Math.floor(Math.random()*100);
-  //   // var j = Math.floor(Math.random()*100);
-  //   // var k = Math.floor(Math.random()*100);
-  //   // gameBoard[i][j][k] = 1;
-  //   // console.log('rando');
-  // }
-  
-  console.log(personToTick,'**');
-  players[personToTick].socket.emit('tick', { now: now, players: _players } );
-  personToTick = (personToTick+1)%players.length;
-  // players[0].socket.volatile.broadcast.emit('tick', { now: now, players: _players } );
+  //send update to next player on next tick
+  personToTick = (personToTick + 1) % players.length;
 }
 
-var personToTick = 0;
-
+//the logical game board
 var gameBoard;
+var gameBoardWidth = 100;
+var gameBoardHeight = 100;
+var gameBoardDepth = 100;
 function setupGameBoard() {
-  gameBoard = new Array(100);
-  for (var i = 0; i < 100; i++) {
-    gameBoard[i] = new Array(100);
-    for (var j = 0; j < 100; j++)
-      gameBoard[i][j] = new Array(100);
+  gameBoard = new Array(gameBoardWidth);
+  for (var i = 0; i < gameBoardWidth; i++) {
+    gameBoard[i] = new Array(gameBoardHeight);
+    for (var j = 0; j < gameBoardHeight; j++)
+      gameBoard[i][j] = new Array(gameBoardDepth);
   }
-  console.log(gameBoard.length);
-  console.log(gameBoard[0].length);
-  console.log(gameBoard[0][0].length);
 }
